4d8a0788d11bf2bce59fa9659ceffa3a5e9323aa
daniBag <117749549+daniBag@users.noreply.github.com>
2024-11-13 11:04:36 +0200

    Update Constants.java mysql pass

--- src/main/java/com/ashcollege/utils/Constants.java
+++ src/main/java/com/ashcollege/utils/Constants.java
@@ -4,5 +4,5 @@ package com.ashcollege.utils;
 public class Constants {
     public static final String SCHEMA = "knowledge_base";
     public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = "Omeriko#1";
+    public static final String DB_PASSWORD = "1234";
 }

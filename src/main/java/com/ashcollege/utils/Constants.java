2507be383a8fc6faccb5af227dc051932c650f1f
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2024-12-04 11:00:48 +0200

    add

--- src/main/java/com/ashcollege/utils/Constants.java
+++ src/main/java/com/ashcollege/utils/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
     public static final String DB_USERNAME = "root";
     public static final String DB_PASSWORD = "1234";
     public static final String SMS_TOKEN = "";
+
 }
